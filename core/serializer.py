45459b891f6b348bd50b27bc4d5fe88be6fedec1
Bruno Barbeiro Moraes <bruno_bmoraes@hotmail.com>
2021-03-06 12:53:42 -0300

    Criando entpoint de items

--- core/serializer.py
+++ core/serializer.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
 from rest_framework.serializers import ListSerializer
 
-from core.models import List
+from core.models import List, Item
+
+
+class ItemSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Item
+        fields = ['name', 'done', ]
 
 
 class ListSerializer(serializers.HyperlinkedModelSerializer):
+    item_set = ItemSerializer(many=True)
+
     class Meta:
         model = List
-        fields = ['name', 'owner', 'url']
+        fields = ['name', 'owner', 'url', 'item_set']
--- core/views.py
+++ core/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework import permissions
-from core.models import List
+from core.models import List, Item
 # Create your views here.
-from core.serializer import ListSerializer
+from core.serializer import ListSerializer, ItemSerializer
 
 
 class ListViewSet(viewsets.ModelViewSet):
-
     queryset = List.objects.all()
     serializer_class = ListSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
     permission_classes = [permissions.IsAuthenticated]
