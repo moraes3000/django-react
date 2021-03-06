f77efdcd747d450158bbc9b39eb4faf3bcba94ba
Bruno Barbeiro Moraes <bruno_bmoraes@hotmail.com>
2021-03-06 14:01:54 -0300

    Configurando CORS no Django e atualizando requirements.txt

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Tutorial do curso Desenvolvimento web com Django, React e deploy AWS e Linux
 
 
+para os serializer funcionar direito devemos instalar esse app para o react puxar os dados
+https://pypi.org/project/django-cors-headers/
--- djangoReact/settings.py
+++ djangoReact/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,12 +35,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # para user api
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
+    # app custon
     'core',
 ]
 
 MIDDLEWARE = [
+    # corsheaders
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    # padrão
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -73,7 +78,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djangoReact.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -83,7 +87,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -103,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -117,8 +119,15 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# lista para acessar o servidor
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
