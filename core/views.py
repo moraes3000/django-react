c8dfe48af1a573d9d2b2ee9159b85756553c3ecb
Bruno Barbeiro Moraes <bruno_bmoraes@hotmail.com>
2021-03-06 13:11:22 -0300

    Ativando o sistema de token

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import permissions, authentication
 from core.models import List, Item
 # Create your views here.
 from core.serializer import ListSerializer, ItemSerializer
@@ -9,10 +9,14 @@ from core.serializer import ListSerializer, ItemSerializer
 class ListViewSet(viewsets.ModelViewSet):
     queryset = List.objects.all()
     serializer_class = ListSerializer
+    # permissao
     permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication]
 
 
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+    # permissao
     permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication]
